8eadb18fa65ea7fae923565b023a3b548a7a4568
AnimeBytes <staff@animebytes.tv>
2019-11-23 00:13:16 +0100

    sync with 163368fdd297503af9fa4a4999cda0399822c882

--- src/Mei/Route/Main.php
+++ src/Mei/Route/Main.php
@@ -24,6 +24,10 @@ class Main extends Base
             });
             $this->post('/delete', DeleteCtrl::class . ':delete')->setName('delete');
             $this->get('/{img:(?:[a-zA-Z0-9]{32}|[a-zA-Z0-9]{64}|[a-zA-Z0-9]{11})(?:-\d{2,3}x\d{2,3}(?:-crop)?)?\.[a-zA-Z]{3,4}}', ServeCtrl::class . ':serve')->setName('serve');
+            $this->get('/images/error.jpg', function ($request, $response, $args) {
+                /** @var Container $this */
+                return $response->withRedirect('/error.jpg')->withStatus(301);
+            });
             $this->get('/images/{img:[a-zA-Z0-9]{32}(?:-\d{2,3}x\d{2,3}(?:-crop)?)?\.[a-zA-Z]{3,4}}', function ($request, $response, $args) { // legacy
                 /** @var Container $this */
                 return $response->withRedirect($this->get('router')->pathFor('serve', ['img' => $args['img']]))->withStatus(301);
