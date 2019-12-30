f90b9f3ca21309ec33a0ea9d60c7b7deb6b30516
AnimeBytes <staff@animebytes.tv>
2019-12-30 20:07:08 +0100

    sync with 0e28579606c650a23c630838b5e2b1d067f9922f

--- public/index.php
+++ public/index.php
@@ -1,12 +1,14 @@
 <?php declare(strict_types=1);
 
 set_error_handler(
-    function ($errno) {
+    function (int $errno, string $errstr, string $errfile, int $errline) {
+        error_log("$errstr ($errno) - $errfile:$errline");
         die('Sorry, something went horribly wrong / PR Environment Error: ' . $errno);
     }
 );
 set_exception_handler(
     function (Throwable $e) {
+        error_log((string)$e);
         die('Sorry, something went horribly wrong / PR Environment Exception: ' . get_class($e));
     }
 );
