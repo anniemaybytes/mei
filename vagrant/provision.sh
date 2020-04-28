245e9f2faf40e956318ecaf46b61b73fdb26f76d
AnimeBytes <staff@animebytes.tv>
2020-04-29 01:49:53 +0200

    sync with 963bb513daabb23392564b0567c3840d409f757c

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ "$EUID" -ne 0 ]; then
+    echo "Must be run as root"
+    exit
+fi
+
 export DEBIAN_FRONTEND=noninteractive
 
 echo
@@ -26,9 +31,9 @@ apt-get -y dist-upgrade
 echo
 echo Installing packages...
 apt-get -y install software-properties-common nginx php7.3 php7.3-xml php7.3-curl php7.3-fpm php7.3-bcmath \
-php7.3-mysqlnd mariadb-server php7.3-common php7.3-gd php7.3-geoip php7.3-imagick phpunit pv php7.3-dev php-pear \
-libcurl3-openssl-dev build-essential php7.3-cli git libpq5 libodbc1 unzip zip libmysqlclient18 libmariadb3 \
-php7.3-mbstring php7.3-json
+    php7.3-mysqlnd mariadb-server php7.3-common php7.3-gd php7.3-geoip php7.3-imagick phpunit pv php7.3-dev php-pear \
+    libcurl3-openssl-dev build-essential php7.3-cli git libpq5 libodbc1 unzip zip libmysqlclient18 libmariadb3 \
+    php7.3-mbstring php7.3-json
 
 pecl install xdebug-2.7.2
 
@@ -39,12 +44,12 @@ rm -f /etc/nginx/sites-enabled/default
 rm -rf /etc/nginx/conf.d
 cd /vagrantroot/configs
 cp -av * /
-chmod 755 /etc/mysql/my.cnf 
+chmod 755 /etc/mysql/my.cnf
 chown -R root:root /etc/mysql/{,mariadb.}conf.d
 chmod -R 644 /etc/mysql/{,mariadb.}conf.d/
 chmod +x /etc/mysql/{,mariadb.}conf.d/
-echo never > /sys/kernel/mm/transparent_hugepage/defrag
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never >/sys/kernel/mm/transparent_hugepage/defrag
+echo never >/sys/kernel/mm/transparent_hugepage/enabled
 
 echo
 echo Installing composer as /usr/local/bin/composer...
@@ -55,10 +60,18 @@ cd /code
 composer install
 
 echo
+echo Configuring daemons...
+systemctl daemon-reload
+systemctl disable nginx
+systemctl disable php7.3-fpm
+systemctl disable mariadb
+
+echo
 echo Stopping daemons...
 systemctl stop nginx
 systemctl stop php7.3-fpm
 systemctl stop mariadb
+systemctl stop cron
 
 echo
 echo Starting daemons...
@@ -66,13 +79,14 @@ systemctl daemon-reload
 systemctl start nginx
 systemctl start php7.3-fpm
 systemctl start mariadb
+systemctl start cron
 
 echo
 echo Configuring MySQL...
 echo "CREATE DATABASE mei; GRANT ALL ON mei.* TO mei@localhost IDENTIFIED BY 'mei';GRANT ALL ON mei.* TO mei@127.0.0.1 IDENTIFIED BY 'mei';GRANT ALL ON mei.* TO 'mei'@'10.0.%.%' IDENTIFIED BY 'mei';" | mysql -uroot
 mysqladmin -uroot password w3llkn0wn
 echo "DELETE FROM user WHERE (User = 'root' AND Host != 'localhost') OR (User = '');FLUSH PRIVILEGES;\q" | mysql -uroot -pw3llkn0wn mysql
-mysql -umei -pmei mei < /vagrantroot/mei.sql
+mysql -umei -pmei mei </vagrantroot/mei.sql
 
 echo
 echo Creating required directories
--- vagrant/provision_update.sh
+++ vagrant/provision_update.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
 set -e
 
+if [ "$EUID" -ne 0 ]; then
+    echo "Must be run as root"
+    exit
+fi
+
 export DEBIAN_FRONTEND=noninteractive
 
 echo
 echo Copying over configs...
 cd /vagrantroot/configs
-cp -av * /
+cp -avu * /
 chmod 755 /etc/mysql/my.cnf
 chown -R root:root /etc/mysql/{,mariadb.}conf.d
 chmod -R 644 /etc/mysql/{,mariadb.}conf.d/
 chmod +x /etc/mysql/{,mariadb.}conf.d/
-echo never > /sys/kernel/mm/transparent_hugepage/defrag
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never >/sys/kernel/mm/transparent_hugepage/defrag
+echo never >/sys/kernel/mm/transparent_hugepage/enabled
+
+echo
+echo Configuring daemons...
+systemctl daemon-reload
+systemctl disable nginx
+systemctl disable php7.3-fpm
+systemctl disable mariadb
 
 echo
 echo Stopping daemons...
 systemctl stop nginx
 systemctl stop php7.3-fpm
 systemctl stop mariadb
+systemctl stop cron
 
 echo
 echo Starting daemons...
-systemctl daemon-reload
 systemctl start nginx
 systemctl start php7.3-fpm
 systemctl start mariadb
-
-cd /code
+systemctl start cron
 
 echo
 echo Migrating
@@ -36,5 +47,6 @@ su -s /bin/bash vagrant -c 'composer phinx migrate'
 
 echo
 echo Updating composer from lock file
+cd /code
 composer self-update
 su -s /bin/bash vagrant -c 'composer install'
